ed4f7d789e1591a4278558a6addd1f803cada932
Steven Mentzer <stevenfmentzer@gmail.com>
2023-12-14 10:44:26 -0500

    finished deliverables

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const technologiesArray = technologies.map((tech) => { 
+    return (
+      <span key={tech}>{tech}</span>
+    )
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologiesArray}
       </div>
     </div>
   );
